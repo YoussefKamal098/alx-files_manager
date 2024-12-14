20283b074d5ca422d53778b81fb44ca38b9f0c69
YoussefKamal098 <kmalywsf098@gmail.com>
2024-12-14 18:48:59 +0200

    Updated authenticate middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,8 +5,13 @@ import redisClient from '../utils/redis';
  */
 const authenticate = async (req, res, next) => {
   const token = req.headers['x-token'];
+  // Regex for match /files/:id/data path
+  const filesIdDataPathRegex = /^\/?files\/([a-fA-F0-9-]{36}|[a-zA-Z0-9_-]+)\/?data\/?$/;
+  const { path } = req;
 
-  if (!token) {
+  if (!token && filesIdDataPathRegex.test(path)) {
+    return res.status(404).json({ error: 'Not found' });
+  } if (!token) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
 
@@ -14,7 +19,9 @@ const authenticate = async (req, res, next) => {
     // Check if the token exists in Redis
     const userId = await redisClient.get(`auth_${token}`);
 
-    if (!userId) {
+    if (!userId && filesIdDataPathRegex.test(path)) {
+      return res.status(404).json({ error: 'Not found' });
+    } if (!userId) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
